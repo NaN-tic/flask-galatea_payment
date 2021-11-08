3223801384ba6605e6c78320c72be4e99c108ed5
Albert Cervera i Areny <albert@nan-tic.com>
2021-11-08 12:10:08 +0100

    Replace TextField with StringField, as it was deprecated in WTF 3.0.0.

--- forms.py
+++ forms.py
@@ -1,13 +1,13 @@
 from flask_babel import lazy_gettext
 from flask_wtf import Form
-from wtforms import TextField, RadioField, validators
+from wtforms import StringField, RadioField, validators
 from wtforms.fields.html5 import DecimalField
 
 
 class PaymentForm(Form):
     '''Payment form'''
     amount = DecimalField(lazy_gettext('Amount'), [validators.Required()])
-    reference = TextField(lazy_gettext('Reference'))
+    reference = StringField(lazy_gettext('Reference'))
     payment_type = RadioField(lazy_gettext('Payment Type'), [validators.Required()])
 
     def __init__(self, *args, **kwargs):
